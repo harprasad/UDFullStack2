ca0495b8bd1aba480a92b2c070f7dacfafeb252f
harprasad <harprasad@ensoimmersive.com>
2018-03-16 16:24:55 +0530

    added semicolons

--- js/app.js
+++ js/app.js
@@ -3,45 +3,45 @@
 * @constructor
 * @param {object} data - The data object which represents a venue(fetched from foursquare)
 */
-var Location = function(data){
+var Location = function (data) {
     var self = this;
     this.name = ko.observable(data.name);
     this.marker = data.marker;
-    this.onItemClicked = function(){
+    this.onItemClicked = function () {
         AnimateMarker(self.marker);
         CreateInfoWindow(self.marker);
-    }
-}
+    };
+};
 
-var ViewModel = function (){
+var ViewModel = function () {
     var self = this;
     this.filtertext = ko.observable("");
     this.errortext = ko.observable("");
     this.locationList = ko.observableArray();
-    this.filteredList = ko.computed(function(){
-        return ko.utils.arrayFilter(self.locationList(),function(location){
-            if(self.filtertext() == ""){
-                ToggleMarker(location.marker,true);
+    this.filteredList = ko.computed(function () {
+        return ko.utils.arrayFilter(self.locationList(), function (location) {
+            if (self.filtertext() === "") {
+                ToggleMarker(location.marker, true);
                 return location;
-            }else{
-                if(location.name().includes(self.filtertext())){
-                    ToggleMarker(location.marker,true);
+            } else {
+                if (location.name().includes(self.filtertext())) {
+                    ToggleMarker(location.marker, true);
                     return location;
-                }else{
-                    ToggleMarker(location.marker,false);
+                } else {
+                    ToggleMarker(location.marker, false);
                 }
             }
         });
     });
-    
-    this.iferror = ko.computed(function(){
-        if(self.errortext() != ""){
+
+    this.iferror = ko.computed(function () {
+        if (self.errortext() !== "") {
             return true;
-        }else{
+        } else {
             return false;
         }
-    })
-}
+    });
+};
 var vm = new ViewModel();
 ko.applyBindings(vm);
 
